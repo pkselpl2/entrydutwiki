d51d8fda486aa050a717b88caac0820c3a943f0b
pkselpl2 <kkammailbox@gmail.com>
2025-12-04 20:21:28 +0900

    Replace HTTP server with Express and CORS
    Removed HTTP server code and added Express and CORS setup.

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-// server.mjs
-import { createServer } from 'node:http';
-
-const server = createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!\n');
-});
-
-// starts a simple http server locally on port 3000
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
-});
-
-// run with `node server.mjs`
 const express = require("express");
 const cors = require("cors");
 const app = express();
